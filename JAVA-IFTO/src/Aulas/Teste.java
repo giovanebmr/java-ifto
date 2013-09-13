238d2314ccbaf57a5bf8f39ad126941e4f704bd1
Giovane Bruno Monte Reis <giovanebmr@gmail.com>
2013-09-13 13:17:49 -0300

    REalizando teste de exportação de um programa para o repositório remoto no GIT HUB

---
+++ JAVA-IFTO/src/Aulas/Teste.java
@@ -0,0 +1,19 @@
+package Aulas;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//package java.ifto;
+
+/**
+ *
+ * @author Giovane
+ */
+public class Teste {
+    
+    public static void main(String [] args){
+        System.out.println("Meu primeiro programa java.");
+    }
+    
+}
---
+++ JAVA-IFTO/src/java/ifto/JAVAIFTO.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java.ifto;
+
+/**
+ *
+ * @author Giovane
+ */
+public class JAVAIFTO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
