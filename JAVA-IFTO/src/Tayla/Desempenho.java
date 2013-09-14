00818f8c319f3330bccfc2519767f547062e0ebc
Tayla <taylapinto@gmail.com>
2013-09-14 18:06:26 -0300

    Arquivo da aula 5 - 14/09/2013

---
+++ JAVA-IFTO/src/Tayla/Desempenho.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tayla;
+
+/**
+ *
+ * @author aluno
+ */
+public class Desempenho{
+  
+  private double nota;
+  private String Mensagem;
+  
+  public String getDesempenho (double nota){ 
+  
+    if (nota >=0 && nota <4){
+    Mensagem = "Aluno com Desempenho Péssimo.";
+    }
+        
+    if (nota >= 4 && nota <6){
+    Mensagem = "Aluno com Desempenho Ruim.";
+    }
+    
+    if (nota >= 6 && nota <8){
+    Mensagem = "Aluno com Desempenho Bom.";
+    }
+    
+    if (nota >= 8 && nota <=10){
+    Mensagem = "Aluno com Desempenho Ótimo.";
+    }
+    
+     if (nota <0 || nota >10){
+    Mensagem = "Informe uma nota entre 0 e 10.";
+    }
+    
+    
+    return Mensagem;
+  }
+ 
+}
+
