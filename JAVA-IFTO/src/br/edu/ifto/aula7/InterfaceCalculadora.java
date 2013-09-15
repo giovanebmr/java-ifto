9f736ed6b808b8cdefe5f347ec2209395625c1d3
Giovane Bruno Monte Reis <giovanebmr@gmail.com>
2013-09-15 14:36:50 -0300

    Exercício de orientação a objetos. Fazer  a implementação da interface fazer um formulario de uma calculadora simples que realize operações básica.

---
+++ JAVA-IFTO/src/br/edu/ifto/aula7/InterfaceCalculadora.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifto.aula7;
+
+/**
+ *
+ * @author Giovane
+ */
+public interface InterfaceCalculadora {
+    
+    double somar();
+    
+    double subtrair();
+    
+    double dividir();
+    
+    double multiplicar();
+    
+}
