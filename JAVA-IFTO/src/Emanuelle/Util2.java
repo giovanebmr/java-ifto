69d65323d4b642cd9f2d2df2d07b29a32b341470
emanuellestriquer <emanuellestriquer@gmail.com>
2013-09-21 16:54:22 -0300

    Arquivo aula 22/09

---
+++ JAVA-IFTO/src/Emanuelle/Util2.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Emanuelle;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author aluno
+ */
+public class Util2 {
+
+    public static void main(String args[]) {
+        int x, y;
+        String s, s2;
+
+
+        Util u = new Util();
+
+        while (!u.isNumero((s = JOptionPane.showInputDialog("Digite valor para s1")))) {
+            JOptionPane.showMessageDialog(null, "Digite apenas numeros!!");
+        }
+        while (!u.isNumero((s2 = JOptionPane.showInputDialog("Digite valor para s2")))) {
+            JOptionPane.showMessageDialog(null, "Digite apenas numeros!!");
+        }
+        x = Integer.parseInt(s);
+        y = Integer.parseInt(s2);
+
+        JOptionPane.showMessageDialog(null, "Soma = " + (x + y));
+    }
+}
