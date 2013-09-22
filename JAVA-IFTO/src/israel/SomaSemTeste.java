f797e507e8e4e789aff7a35432747b1efab69674
Giovane Bruno Monte Reis <giovanebmr@gmail.com>
2013-09-22 03:04:43 -0300

    alterei o package dos

--- JAVA-IFTO/src/israel/SomaSemTeste.java
+++ JAVA-IFTO/src/israel/SomaSemTeste.java
@@ -1,8 +1,10 @@
+package israel;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.edu.ifto.aula8;
+
 
 import javax.swing.JOptionPane;
 
--- JAVA-IFTO/src/israel/TesteUtil.java
+++ JAVA-IFTO/src/israel/TesteUtil.java
@@ -2,7 +2,8 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.edu.ifto.aula8;
+package israel;
+
 
 import javax.swing.JOptionPane;
 
