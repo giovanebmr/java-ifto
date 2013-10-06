06b6fc760fa943415592af3b3426c597838011e1
Nayron dos Anjos <nayron1002010@hotmail.com>
2013-10-06 18:06:41 -0300

    Arquivo aula 06

--- JAVA-IFTO/src/Nayron_dos_anjos/aula7/banco/JogadorDao.java
+++ JAVA-IFTO/src/Nayron_dos_anjos/aula7/banco/JogadorDao.java
@@ -38,6 +38,30 @@ public class JogadorDao extends DBConnection {
 
     }
 
+    public void deleteUsuario(int id) throws SQLException {
+        String SQL = "DELETE FROM NAYRON.jogador WHERE id=?";
+
+        PreparedStatement stmt = conn.prepareStatement(SQL);
+
+        stmt.execute();
+        stmt.close();
+    }
+
+    public void AlterarUsuario(Jogador jogador) throws SQLException {
+        String SQL = "UPDATE NAYRON.jogador SET nome=?,login=?,email=?,senha=?,WHERE id=?";
+
+        PreparedStatement stmt = conn.prepareStatement(SQL);
+
+        stmt.setString(1, jogador.getNome());
+        stmt.setString(2, jogador.getLogin());
+        stmt.setString(3, jogador.getEmail());
+        stmt.setString(4, jogador.getSenha());
+        stmt.setInt(5, jogador.getId());
+
+        stmt.execute();
+        stmt.close();
+    }
+
     /**
      *
      * @return @throws SQLException
@@ -49,28 +73,27 @@ public class JogadorDao extends DBConnection {
         ResultSet rs;
 
         List jogadores = new LinkedList<>();
-       
+
         stmt = conn.prepareStatement(sql);
         rs = stmt.executeQuery();
-        
-        while(rs.next()){
+
+        while (rs.next()) {
             jogadores.add(getObjeto(rs));
         }
 
         return jogadores;
     }
-    
-    public Jogador getObjeto(ResultSet rs) throws SQLException{
-        
+
+    public Jogador getObjeto(ResultSet rs) throws SQLException {
+
         Jogador jogador = new Jogador();
-        
+
         jogador.setid(rs.getInt("id"));
         jogador.setNome(rs.getString("nome"));
         jogador.setEmail(rs.getString("email"));
-      //  jogador.setSenha(rs.getString("senha"));
-        
+        //  jogador.setSenha(rs.getString("senha"));
+
         return jogador;
-    
+
     }
-    
 }
