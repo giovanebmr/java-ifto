9dc08fd1293c0673317bec563c5e07153b3e4082
Náyron dos Anjos <nayron1002010@hotmail.com>
2013-10-07 22:30:35 -0300

    aulas de sabado dia 05 e domingo dia 06

---
+++ JAVA-IFTO/src/Nayron_dos_anjos/aula7/bancodedados/JogadorDao.java
@@ -0,0 +1,76 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Nayron_dos_anjos.aula7.bancodedados;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ *
+ * @author aluno
+ */
+public class JogadorDao extends DBConnection {
+
+    private final Connection conn;
+
+    public JogadorDao() throws ClassNotFoundException, SQLException {
+        this.conn = this.getMyDBConnection();
+    }
+
+    public void addJogador(Jogador jogador) throws SQLException {
+        String SQL = "insert into NAYRON.jogador(nome,login,email,senha) values (?,?,?,?)";
+
+        PreparedStatement stmt = conn.prepareStatement(SQL);
+
+        stmt.setString(1, jogador.getNome());
+        stmt.setString(2, jogador.getLogin());
+        stmt.setString(3, jogador.getEmail());
+        stmt.setString(4, jogador.getSenha());
+
+        stmt.execute();
+        stmt.close();
+
+    }
+
+    /**
+     *
+     * @return @throws SQLException
+     */
+    public List<Jogador> listaUsuarios() throws SQLException {
+
+        String sql = "SELECT * FROM Jogo.jogador";
+        PreparedStatement stmt;
+        ResultSet rs;
+
+        List jogadores = new LinkedList<>();
+       
+        stmt = conn.prepareStatement(sql);
+        rs = stmt.executeQuery();
+        
+        while(rs.next()){
+            jogadores.add(getObjeto(rs));
+        }
+
+        return jogadores;
+    }
+    
+    public Jogador getObjeto(ResultSet rs) throws SQLException{
+        
+        Jogador jogador = new Jogador();
+        
+        jogador.setid(rs.getInt("id"));
+        jogador.setNome(rs.getString("nome"));
+        jogador.setEmail(rs.getString("email"));
+      //  jogador.setSenha(rs.getString("senha"));
+        
+        return jogador;
+    
+    }
+    
+}
